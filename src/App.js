e97c99902a7b4f972db9f6109a45bbb6d0b15530
khalidkao <khalidkao@example.com>
2023-12-08 18:21:08 +0100

    some changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import './App.css'
-import FilterableSelectors from "./FilterableSelectors"; // Make sure to provide the correct path to your FilterableSelectors component
+import RendezVous from "./RendezVous"; // Make sure to provide the correct path to your FilterableSelectors component
 
 function App() {
   return (
     <div className="App">
-      <FilterableSelectors />
+      <RendezVous />
     </div>
   );
 }
